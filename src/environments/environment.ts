bfe0cec4a751871ded53ebab47d5c5882345720f
prashanttathe <70051910+prashanttathe@users.noreply.github.com>
2020-12-16 09:06:59 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -23,5 +23,5 @@
 
 export const environment = {
   production: false,
-  REST_API_URL: 'http://petclinic-api.e46708b92c054086909b.eastus.aksapp.io/petclinic/api/'
+  REST_API_URL: 'http://petclinicapi.e46708b92c054086909b.eastus.aksapp.io/petclinic/api/'
 };
